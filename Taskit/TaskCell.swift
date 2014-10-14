0fe848e870d6396d6912ff6ea5234c32575cc0d6
darnoldi <davearnoldi@Daves-iMac.local>
2014-10-14 13:31:25 +0200

    Taskit - initial

---
+++ Taskit/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Taskit
+//
+//  Created by Dave Arnoldi on 2014/10/13.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
