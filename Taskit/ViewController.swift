cb15f8ca171814fd75982ce71fcdb42fe26aa495
darnoldi <davearnoldi@Daves-iMac.local>
2014-10-16 12:54:15 +0200

    Taskit adding tasks

--- Taskit/ViewController.swift
+++ Taskit/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+   
+    var taskArray:[Dictionary<String,String>] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let task1:Dictionary<String,String> = ["task": "Study French", "subtask": "parle vouz?", "date": "01/14/2014"]
+        let task2:Dictionary<String,String> = ["task": "eat dinner", "subtask": "burgers", "date": "01/14/2014"]
+        let task3:Dictionary<String,String> = ["task": "Play Guitar", "hard rock": "parle vouz?", "date": "01/16/2014"]
+        let task4:Dictionary<String,String> = ["task": "chill out", "subtask": "zzzzzzz", "date": "01/17/2014"]
+        
+        taskArray = [task1, task2, task3, task4]
+        
+        //tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,11 +37,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //UItableViewDataSource
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 4
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        
+        let taskDict:Dictionary = taskArray[indexPath.row]
+        
+        
+        cell.taskLabel.text = taskDict["task"]
+        cell.descriptionLabel.text = taskDict["subtask"]
+        cell.dateLabel.text = taskDict["date"]
+        
+        
+        return cell
     }
     
     
